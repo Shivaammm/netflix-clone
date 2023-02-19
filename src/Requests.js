a3c9e4b25e92ede6df5df68ab246d30e6fca1c02
shivvvvvv <88606731+shivvvvvv@users.noreply.github.com>
2023-02-19 23:33:13 +0530

    cover page

--- src/Requests.js
+++ src/Requests.js
@@ -1,11 +1,11 @@
 const key = 'b7b7d75659538f512efdae15b82753f4'
 
 const requests = {
-    requestPopular: 'https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1',
-    requestTrending: 'https://api.themoviedb.org/3/trending/all/day?api_key=${key}',
-    requestLatest: 'https://api.themoviedb.org/3/movie/latest?api_key=${key}&language=en-US',
-    requestUpcoming: 'https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=en-US&page=1',
-    requestTopRated: 'https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=1',
+    requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`,
+    requestTrending: `https://api.themoviedb.org/3/trending/all/day?api_key=${key}`,
+    requestLatest: `https://api.themoviedb.org/3/movie/latest?api_key=${key}&language=en-US`,
+    requestUpcoming: `https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=en-US&page=1`,
+    requestTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=1`,
 
 };
 
