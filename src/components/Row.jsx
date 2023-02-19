3fdb2a9ba92e013c7975ea51f21c9de2e685a218
shivvvvvv <88606731+shivvvvvv@users.noreply.github.com>
2023-02-20 00:20:52 +0530

    Scroll functionality

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -22,6 +22,7 @@ const Row = ({ title, fetchURL, rowID }) => {
   };
 
   return (
+   
     <>
       <h2 className='text-white font-bold md:text-xl p-4'>{title}</h2>
       <div className='relative flex items-center group'>
